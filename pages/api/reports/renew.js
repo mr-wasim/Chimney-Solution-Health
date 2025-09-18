7a312fcbe5bc3c8decb573f33f032e64ab378549
Wasim <wasim@example.com>
2025-09-18 14:37:30 +0530

    kt

--- pages/api/reports/renew.js
+++ pages/api/reports/renew.js
@@ -1,5 +1,5 @@
-import dbConnect from "../../lib/mongodb";
-import Report from "../../models/Report";
+import dbConnect from "../../../lib/mongodb";
+import Report from "../../../models/Report";
 
 export default async function handler(req, res) {
   await dbConnect()
