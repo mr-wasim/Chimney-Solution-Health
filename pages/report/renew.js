a0d688fdbdde1bc4d6cfc2a3157b47e6cd18f630
Wasim <wasim@example.com>
2025-09-18 14:31:56 +0530

    fix import paths

--- pages/report/renew.js
+++ pages/report/renew.js
@@ -1,5 +1,5 @@
-import dbConnect from '../../lib/mongodb.js'
-import Report from '../../models/Report.js'
+import dbConnect from "../../lib/mongodb";
+import Report from "../../models/Report";
 
 export default async function handler(req, res) {
   await dbConnect()
